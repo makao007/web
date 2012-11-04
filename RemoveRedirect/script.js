adca5cc41076b3e72868b16a10eaf1853476dfa1
makao007@126.com <makao007@126.com>
2012-11-04 14:11:57 +0800

    add a useful tool which can remove url redirect from google search result

---
+++ RemoveRedirect/script.js
@@ -0,0 +1,17 @@
+var url = window.location.href.toLowerCase(); 
+//alert(url);
+if (url.indexOf("www.google.com.hk") >= 0 || url.indexOf("www.google.com") >= 0 || url.indexOf("/search") >= 0) 
+{ 
+    // var all = document.querySelectorAll("*");  origin 
+    var all = document.getElementById("search").getElementsByTagName("a");       //updated
+    for (var i = 0; i < all.length; i ++) 
+    { 
+        all[i].onmousedown = null; 
+		all[i].setAttribute("onmousedown", " ");
+		/*
+		all[i].onmousedown = function(){
+			alert("nowamagic");	
+		}
+		*/
+    }
+}
