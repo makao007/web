0a15ca0752ede5ee40a135f28264838f1113acc4
makao007 <makao007@126.com>
2015-03-25 23:23:43 +0800

    laravel b/e edit

---
+++ php_laravel/AdminController/CategoryController.php
@@ -0,0 +1,131 @@
+<?php namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use App\Category as Record;
+use View;
+use Input;
+use Redirect;
+
+class CategoryController extends Controller {
+
+    protected $link = 'admin/category';
+    protected $name = 'category';
+    protected $path = 'admin.category.';
+
+    public function __construct()
+    {
+        View::share('link', $this->link);
+    }
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+        $records = Record::paginate(5);
+        return view($this->path . 'index', ['records'=>$records ]);
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+        return view($this->path . 'create');
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store(Request $request)
+	{
+        /*
+        $this->validate($request, [
+            'title' => 'required|unique:pages|max:255',
+            'body' => 'required',
+        ]);
+        */
+
+        $record = new Record;
+        $record->name = Input::get('name');
+        $record->remark = Input::get('remark');
+
+        if ($record->save()) {
+            return Redirect::to($this->link);
+        } else {
+            return Redirect::back()->withInput()->withErrors('保存失败！');
+        }
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+        return view($this->path . 'edit', ['record' => Record::find($id)] );
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update(Request $request, $id)
+	{
+        /*
+        $this->validate($request, [
+            'title' => 'required|unique:pages,title,'.$id.'|max:255',
+            'body' => 'required',
+        ]);
+        */
+
+        $record = Record::find($id);
+        $record->name = Input::get('name');
+        $record->remark  = Input::get('remark');
+
+        if ($record->save()) {
+            return Redirect::to($this->link);
+        } else {
+            return Redirect::back()->withInput()->withErrors('保存失败！');
+        }
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+        $record = Record::find($id);
+        $record->delete();
+
+        return Redirect::to($this->link);
+	}
+
+}
